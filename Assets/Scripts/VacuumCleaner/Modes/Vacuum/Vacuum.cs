a9b70715456eca62b139a36b63121f73eca41f40
MateoMonastra <mateomonastravm@gmail.com>
2024-09-25 05:23:46 -0300

    Upgrade the trashcollector feeling

--- Assets/Scripts/VacuumCleaner/Modes/Vacuum/Vacuum.cs
+++ Assets/Scripts/VacuumCleaner/Modes/Vacuum/Vacuum.cs
@@ -9,16 +9,19 @@ namespace VacuumCleaner.Modes
         public event Action OnPowerOff;
         
         [SerializeField] private VacuumColision vacuumColision;
+        [SerializeField] private TrashCollector trashCollector;
         
         public void PowerOn()
         {
             vacuumColision.gameObject.SetActive(true);
+            trashCollector.gameObject.SetActive(true);
             OnPowerOn?.Invoke();
         }
 
         public void PowerOff()
         {
             vacuumColision.gameObject.SetActive(false);
+            trashCollector.gameObject.SetActive(false);
             OnPowerOff?.Invoke();
         }
     }
--- Assets/Scripts/VacuumCleaner/Modes/Vacuum/VacuumColision.cs
+++ Assets/Scripts/VacuumCleaner/Modes/Vacuum/VacuumColision.cs
@@ -1,20 +1,38 @@
+using System;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace VacuumCleaner.Modes
 {
     public class VacuumColision : MonoBehaviour
     {
         [SerializeField] private Transform target;
-        [SerializeField] private VacuumModel _model;
+        [SerializeField] private VacuumModel model;
         
         private Ray _ray;
+        
+        private Vector3? _collision;
+        private Quaternion _left;
+        private Quaternion _right;
+
+        private Vector3 _leftBoundary;
+        private Vector3 _rightBoundary;
+
+        private void Start()
+        {
+            _left = Quaternion.AngleAxis(-model.MaxAngle, Vector3.up);
+            _right = Quaternion.AngleAxis(model.MaxAngle, Vector3.up);
+            _leftBoundary = _left * target.forward;
+            _rightBoundary = _right * target.forward;
+        }
+
         private void OnTriggerStay(Collider other)
         {
             if (IsVacuumable(other))
             {
                 if (CanVacuum(other))
                 {
-                    other.GetComponent<IVacuumable>().IsBeingVacuumed(target.position, _model.Speed);
+                    other.GetComponent<IVacuumable>().IsBeingVacuumed(target.position, model.Speed);
                 }
             }
         }
@@ -30,16 +48,39 @@ namespace VacuumCleaner.Modes
         {
             var angleToObject = Vector3.Angle(target.forward, other.transform.position - target.position);
 
-            if (!(angleToObject <= _model.MaxAngle)) return false;
+            if (!(angleToObject <= model.MaxAngle)) return false;
 
             _ray = new Ray(target.position, other.transform.position - target.position);
 
-            if (Physics.Raycast(_ray, out var hit, _model.RenderDistance, _model.WallLayer))
+            if (Physics.Raycast(_ray, out var hit, model.RenderDistance, model.WallLayer))
             {
                 return false;
             }
                 
             return true;
         }
+        
+#if UNITY_EDITOR
+
+        private void OnDrawGizmos()
+        {
+            _left = Quaternion.AngleAxis(-model.MaxAngle, Vector3.up);
+            _right = Quaternion.AngleAxis(model.MaxAngle, Vector3.up);
+            _leftBoundary = _left * target.forward;
+            _rightBoundary = _right * target.forward;
+
+            Gizmos.color = Color.green;
+            Gizmos.DrawLine(target.position, target.position + target.forward * model.RenderDistance);
+
+            Gizmos.DrawLine(target.position, target.position + _leftBoundary * model.RenderDistance);
+
+            Gizmos.DrawLine(target.position, target.position + _rightBoundary * model.RenderDistance);
+            Gizmos.DrawRay(_ray);
+            if (_collision != null)
+            {
+                Gizmos.DrawSphere(_collision.Value, 0.5f);
+            }
+        }
+#endif
     }
 }
