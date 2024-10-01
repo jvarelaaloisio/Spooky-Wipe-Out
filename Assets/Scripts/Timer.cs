a54714155f84f331951e0cc85967907a9ef96f93
MateoMonastra <mateomonastravm@gmail.com>
2024-10-01 14:50:35 -0300

    Add timer

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,52 @@
+using TMPro;
+using UnityEngine;
+
+namespace Gameplay.Timer
+{
+    enum CountType 
+    {
+        CountDown,
+        CountUp
+
+    };
+
+    public class Timer : MonoBehaviour
+    {
+        public TextMeshProUGUI textTimer;
+        [SerializeField] private float startTime;
+        [SerializeField] private CountType countType;
+
+        private float _timer;
+        public float TotalTime => _timer;
+
+        private void Start()
+        {
+            _timer = startTime;
+        }
+
+        private void Update()
+        {
+            if(countType == CountType.CountUp)
+            {
+            _timer += Time.deltaTime;
+            }
+            else if (countType == CountType.CountDown) 
+            {
+            _timer -= Time.deltaTime;
+            }
+
+            int minutes = (int)_timer / 60;
+            int seconds = (int)_timer % 60;
+
+            if (seconds >= 10)
+            {
+                textTimer.text = minutes + ":" + seconds;
+            }
+            else
+            {
+                textTimer.text = minutes + ":0" + seconds;
+            }
+        }
+
+    }
+}
