ccee98a478f5c9b91c48d2722b9b092597e56546
Porte <portepetit01@gmail.com>
2024-10-04 03:46:00 -0300

    Update see through shader & assets

--- Assets/Scripts/CircleSync.cs
+++ Assets/Scripts/CircleSync.cs
@@ -1,32 +1,70 @@
-using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
-using UnityEngine.UIElements;
 
 public class CircleSync : MonoBehaviour
 {
     public static int PosID = Shader.PropertyToID("_Player_Position");
     public static int SizeID = Shader.PropertyToID("_Size");
 
-    public Material Wall_Material;
+    public List<Material> Materials;
+
     public Camera Camera;
     public LayerMask Mask;
-    public float Speed;
+    public float raycastDistance = 5;
+    float alpha;
+    Ray ray;
+
+
+    [Tooltip("The speed until the effect appears or disappears")]
+    public float speedAlpha;
+
 
     private void Update()
     {
         var dir = Camera.transform.position - transform.position;
-        var ray = new Ray(transform.position, dir.normalized);
+        ray = new Ray(transform.position, dir.normalized);
+        //ray = new Ray(transform.position, Quaternion.AngleAxis(-45, Vector3.up) * -Vector3.forward);
 
-        if (Physics.Raycast(ray, 6000, Mask))
-            Wall_Material.SetFloat(SizeID, 1);
-        
+        if (Physics.Raycast(ray,out var hit, raycastDistance, Mask ))
+        {
+            foreach (Material material in Materials)
+            {
+                
+                alpha += Time.deltaTime * speedAlpha;
+                alpha = Mathf.Clamp01(alpha);
+                material.SetFloat(SizeID, alpha);
+            }
+        }
+
+
         else
-            Wall_Material.SetFloat(SizeID, 0);
+        {
+            
+            alpha -= Time.deltaTime * speedAlpha;
+            alpha = Mathf.Clamp01(alpha);
+            foreach (Material material in Materials)
+            {
+
+                material.SetFloat(SizeID, alpha);
+            }
+        }
 
         var view = Camera.WorldToViewportPoint(transform.position);
-        Wall_Material.SetVector(PosID, view);
-        
+        foreach (Material material in Materials)
+        {
+            material.SetVector(PosID, view);
+        }
 
+
+
+    }
+    private void OnDrawGizmos()
+    {
+        if (Application.isPlaying)
+        {
+          Gizmos.color = Color.yellow;
+            Gizmos.DrawRay(ray);  
+        }
     }
 }
