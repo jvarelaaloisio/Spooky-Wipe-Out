c06b7fc35544a5e37c853e056a2b0660dd349085
Caro <carolinapohn@gmail.com>
2024-10-14 23:54:29 -0300

    Add ghost patrolling (WIP)

---
+++ Assets/Scripts/AI/DecisionTree/Actions/Action_Patrolling.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Action_Patrolling : AI.DecisionTree.TreeAction
+{
+    public override void NodeFunction()
+    {
+        throw new System.NotImplementedException();
+    }
+}
--- Assets/Scripts/AI/DecisionTree/Questions/CanSeeHunter.cs
+++ Assets/Scripts/AI/DecisionTree/Questions/CanSeeHunter.cs
@@ -1,23 +1,27 @@
 using UnityEngine;
 using Ghosts;
+
 public class CanSeeHunter : AI.DecisionTree.TreeQuestion
 {
     public override int CheckCondition()
     {
         var hunter = GameObject.FindGameObjectWithTag("Player");
+
         if (hunter == null)
         {
             return 1;
         }
+
         if(getData() is GhostBehaviourData data)
         {
             // set using data.self.<<something>>
-            float viewDistance = 0;
+
+            float viewDistance = 5;
             var result = Vector3.Distance(data.hunter.position.IgnoreY(), data.self.transform.position.IgnoreY()) < viewDistance;
             return result ? 0 : 1;
         }
-        return 1;
 
+        return 1;
     }
 }
 public struct GhostBehaviourData
