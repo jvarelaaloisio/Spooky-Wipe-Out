eda68d590b9e15f4c122602d901d9a6b372377f4
MateoMonastra <mateomonastravm@gmail.com>
2024-10-29 21:59:18 -0300

    Add new art in pause menu

--- Assets/Scripts/Game/Minigames/AD/AD.cs
+++ Assets/Scripts/Game/Minigames/AD/AD.cs
@@ -1,15 +1,18 @@
 using UnityEngine;
+using UnityEngine.Serialization;
 using UnityEngine.UI;
 
 namespace Minigames
 {
     public class AD : MonoBehaviour
     {
-        [SerializeField] private Image progressBar;
+        [SerializeField] private Image rightProgressBar;
+        [SerializeField] private Image leftProgressBar;
 
         public void SetProgressBarFill( float value )
         {
-            progressBar.fillAmount = value;
+            rightProgressBar.fillAmount = value;
+            leftProgressBar.fillAmount = value;
         }
     }
 }
