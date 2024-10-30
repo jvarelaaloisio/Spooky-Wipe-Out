0960c73bcce8081b29776445605324fdd718945d
Caro <carolinapohn@gmail.com>
2024-10-30 01:56:11 -0300

    Add sprite animation for AD minigame

--- Assets/Scripts/Game/Minigames/AD/AD.cs
+++ Assets/Scripts/Game/Minigames/AD/AD.cs
@@ -9,10 +9,14 @@ namespace Minigames
         [SerializeField] private Image rightProgressBar;
         [SerializeField] private Image leftProgressBar;
 
+        [SerializeField] private Animator animator;
+
         public void SetProgressBarFill( float value )
         {
             rightProgressBar.fillAmount = value;
             leftProgressBar.fillAmount = value;
+
+            animator?.SetBool("isScared", value < 0.5f);
         }
     }
 }
